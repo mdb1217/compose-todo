f51392bdc06791b0df589106024035f260d84678
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-03-04 00:23:12 +0900

    [FEAT] Init TodoItem

--- app/src/main/java/org/sopt/compose_todo/MainActivity.kt
+++ app/src/main/java/org/sopt/compose_todo/MainActivity.kt
@@ -3,13 +3,19 @@ package org.sopt.compose_todo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import org.sopt.compose_todo.model.ToDo
 import org.sopt.compose_todo.ui.theme.ComposetodoTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,6 +38,20 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
+}
+
+@Composable
+fun TodoItem(todo : ToDo) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = ""
+        )
+        Column {
+            Text(todo.title)
+            Text(todo.content)
+        }
+    }
 }
 
 @Preview(showBackground = true)

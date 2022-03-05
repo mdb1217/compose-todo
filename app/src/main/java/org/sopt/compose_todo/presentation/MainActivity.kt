d534a1e19139720b0bc3d835e9ef69af4be49626
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-03-05 11:59:06 +0900

    [CHORE] Modify Overall Code

--- app/src/main/java/org/sopt/compose_todo/presentation/MainActivity.kt
+++ app/src/main/java/org/sopt/compose_todo/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package org.sopt.compose_todo
+package org.sopt.compose_todo.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -7,6 +7,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -14,7 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import org.sopt.compose_todo.R
 import org.sopt.compose_todo.model.ToDo
 import org.sopt.compose_todo.ui.theme.ComposetodoTheme
 
@@ -28,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    // CreateList(list = )
                 }
             }
         }
@@ -36,12 +38,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CreateList(list: List<ToDo>) {
+    LazyColumn {
+        items(list) { item ->
+            TodoItem(todo = item)
+        }
+    }
 }
 
 @Composable
-fun TodoItem(todo : ToDo) {
+fun TodoItem(todo: ToDo) {
     Row(verticalAlignment = Alignment.CenterVertically) {
         Image(
             painter = painterResource(id = R.drawable.ic_launcher_foreground),
@@ -51,13 +57,5 @@ fun TodoItem(todo : ToDo) {
             Text(todo.title)
             Text(todo.content)
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposetodoTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/org/sopt/compose_todo/presentation/viewmodel/MainViewModel.kt
@@ -0,0 +1,7 @@
+package org.sopt.compose_todo.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+}
